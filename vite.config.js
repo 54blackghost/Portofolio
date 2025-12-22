23879d1fab44ae8e2735216d7313f1cb31058eed
unknown <ulrichleblack@gmail.com>
2025-12-22 20:42:58 +0100

    reconvertion vers react js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [vue()],
-  server: {
-    port: 5173, // ou un autre port de ton choix
-  },
-  build: {
-    assetsInlineLimit: 0, // Ensure assets are not inlined
-  },
-  publicDir: 'public', // Explicitly set the public directory
-});
+  plugins: [react()],
+})
